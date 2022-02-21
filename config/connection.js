6dfda95458bc356e3d253092f9f8d4867b26b913
coyote <megan.metelak@gmail.com>
2022-02-21 14:53:35 -0600

    added .env and updated models

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
-
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
-
+// create connection to our database, pass in your MySQL information for username and password
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -10,6 +10,24 @@ const sequelize = process.env.JAWSDB_URL
       dialectOptions: {
         decimalNumbers: true,
       },
+      // port: 3306
     });
 
 module.exports = sequelize;
+
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+// let sequelize;
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     dialectOptions: {
+//       decimalNumbers: true,
+//     },
+//     port: 3306
+//   });
+// }
+// module.exports = sequelize;
